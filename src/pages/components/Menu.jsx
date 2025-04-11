5b2433f416667c9d2408a51ecc91f59cbdf08b43
nomuluun <nomukaolnoo223@gmail.com>
2025-04-11 11:04:19 +0800

    s

--- src/pages/components/Menu.jsx
+++ src/pages/components/Menu.jsx
@@ -6,17 +6,17 @@ import { useRouter } from "next/router";
 export default function Menu() {
   const router = useRouter();
   return (
-    <div className="h-[110px]  bg-[#F9BC60] w-[100%] flex justify-center items-center">
-      <div className="h-[110px] max-w-[500px] w-[100%] flex flex-row justify-around items-end pb-[5px]">
-        <button onClick={() => router.push("/info")}>
-          <Profile />
+    <div className="h-[] p-8 bg-[#F9BC60] w-[100%] flex justify-center items-center">
+      <div className="h-[] max-w-[500px] w-[100%] flex flex-row justify-around items-end pb-[5px]">
+        <button onClick={() => router.push("/")}>
+          <Home />
         </button>
         <Gal />
         <button onClick={() => router.push("/scoreboard")}>
           <Rank />
         </button>
-        <button onClick={() => router.push("/")}>
-          <Home />
+        <button onClick={() => router.push("/info")}>
+          <Profile />
         </button>
       </div>
     </div>
--- src/pages/components/ModuleUtga.jsx
+++ src/pages/components/ModuleUtga.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function ModuleUtga() {
   return (
-    <div className="w-[80%] border bg-[#F9BC60] p-10 rounded-[30px] flex flex-col justify-around gap-[20px] pl-[20px] shadow-2xl">
+    <div className="w-[100%] max-w-[450px] border bg-[#F9BC60] p-10 rounded-[30px] flex flex-col justify-around gap-[20px] pl-[20px] shadow-2xl">
       <Link href="/ugutga">
         <h1 className="text-[#004643] font-black text-[20px]">ҮГИЙН УТГА</h1>
         <h3 className="text-[#FFFFFF] font-black text-[15px] pt-[10px] ">
